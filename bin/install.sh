37a8065b43079a0d3fb7f575d7fd809a29191818
Yury Antonov <yantonov@yandex.ru>
2015-02-12 11:29:59 +0500

    check jdk, mvn, lein before install dependencies

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-mvn_output=`mvn --version | grep "Apache Maven"`
-if [ -z "$mvn_output" ]; then
-    echo "mvn not found";
-    echo "you can get apache maven here: http://maven.apache.org"
+output=`java -version 2>&1 >/dev/null | grep "Java(TM) SE Runtime Environment"`
+if [ -z "$output" ]; then
+    echo "[ERROR] - java not found";
+    echo "[INFO] - you can get it here: www.oracle.com/technetwork/java/javase/downloads/index.html"
+    exit 0
+fi
+
+output=`mvn --version | grep "Apache Maven"`
+if [ -z "$output" ]; then
+    echo "[ERROR] - mvn not found";
+    echo "[INFO] - you can it here: http://maven.apache.org"
+    exit 0
+fi
+
+output=`lein -version | grep "Leiningen"`
+if [ -z "$output" ]; then
+    echo "[ERROR] - lein not found";
+    echo "[INFO] - you can get it here: http://leiningen.org"
     exit 0
 fi
 
