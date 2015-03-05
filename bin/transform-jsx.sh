e6d18c166b216f66ebb90f2b4ff1f6de677800d9
Yury Antonov <yantonov@yandex.ru>
2015-03-06 01:04:40 +0500

    in case of jsx is not installed - print corresponding message

--- bin/transform-jsx.sh
+++ bin/transform-jsx.sh
@@ -2,4 +2,12 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
+output=`jsx --help | grep "Usage:"`
+if [ -z "$output" ]; then
+    echo "[ERROR] - jsx not found";
+    echo "[INFO] - you can install it using node js package manager: "
+    echo "\$> npm install -g react-tool"
+    exit 0
+fi
+
 jsx --extension jsx $SCRIPT_DIR/../resources/public/js $SCRIPT_DIR/../resources/public/js/combined
--- bin/watch-jsx.sh
+++ bin/watch-jsx.sh
@@ -2,5 +2,13 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
+output=`jsx --help | grep "Usage:"`
+if [ -z "$output" ]; then
+    echo "[ERROR] - jsx not found";
+    echo "[INFO] - you can install it using node js package manager: "
+    echo "\$> npm install -g react-tool"
+    exit 0
+fi
+
 jsx --extension jsx --watch $SCRIPT_DIR/../resources/public/js $SCRIPT_DIR/../resources/public/js/combined
 
