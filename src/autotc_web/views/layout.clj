7805ca9d6e2c7cfb8bd527a4d567096f8c4c1b6c
Yury Antonov <yantonov@yandex.ru>
2015-03-20 00:31:39 +0500

    migrate to bootstrap 3.3.4

--- src/autotc_web/views/layout.clj
+++ src/autotc_web/views/layout.clj
@@ -10,7 +10,7 @@
     (include-js "/js/common/String.js")
     (include-js "/js/common/Console.js")
     (include-js "/js/common/Poller.js")
-    (include-js "/js/lib/bootstrap.3.3.2.min.js")
+    (include-js "/js/lib/bootstrap.3.3.4.min.js")
     (include-js "/js/lib/react-0.13.1.js")
     (include-js "/js/lib/react-bootstrap.min.js")
     [:link {:rel "shortcut icon"
