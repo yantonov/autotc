b67050867eace272bc9191fa87b90b04902ad7cd
Yury Antonov <yantonov@yandex.ru>
2015-08-21 19:14:15 +0500

    bumped up clojure version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject autotc-web "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
                  [ring-server "0.4.0"]
