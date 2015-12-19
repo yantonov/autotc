b6004f5263ee59a6385b03845f40a5bbdccd8610
Yury Antonov <yantonov@yandex.ru>
2015-12-19 13:23:26 +0500

    automate cljs compile generation

---
+++ bin/generate-production-js.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+
+cd $SCRIPT_DIR/../
+
+lein cljsbuild once production
+cp target/autotc-web.js resources/public/cljs/dev/
+
+echo "generation cljs -> js has completed!"
