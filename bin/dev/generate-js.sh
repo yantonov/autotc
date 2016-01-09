32cb00615f4bc306407835263edd940fae3adfb0
Yury Antonov <yantonov@yandex.ru>
2016-01-09 13:05:22 +0500

    clean old files before generate js from cljs

--- bin/dev/generate-js.sh
+++ bin/dev/generate-js.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
 cd $SCRIPT_DIR/../../
 
+# clean all old cljs files
+rm -rf resources/public/cljs/dev
+# regenerate new files
 lein cljsbuild once development
-# cp target/autotc-web.js resources/public/cljs/dev/
 
 echo "generation cljs -> js has completed!"
