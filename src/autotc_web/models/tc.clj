d2415a0b3ada12eab1622812904c511dd49d1690
Yury Antonov <yantonov@yandex.ru>
2016-01-15 10:17:28 +0500

    logs for retrive build type info

--- src/autotc_web/models/tc.clj
+++ src/autotc_web/models/tc.clj
@@ -1,5 +1,7 @@
 (ns autotc-web.models.tc
-  (:require [clj-teamcity-api.core :as tc])
+  (:require [clj-teamcity-api.core :as tc]
+            [autotc-web.log :as log]
+            [autotc-web.models.exception :as exception])
   (:import clj_teamcity_api.core.TeamCityServer)
   (:import clj_teamcity_api.core.Credentials))
 
@@ -10,18 +12,21 @@
         project (tc/project server credentials project-id)
         build-type-ids (:build-type-ids project)
         build-types (pmap (fn [build-type-id]
-                            (let [last-build (->> build-type-id
-                                                  (tc/last-builds server credentials)
-                                                  first)
-                                  build-type (->> build-type-id
-                                                  (tc/build-type server credentials)
-                                                  :attrs)
-                                  last-build-details (->> last-build
-                                                          :id
-                                                          (tc/build server credentials))]
-                              {:last-build last-build
-                               :build-type build-type
-                               :last-build-details last-build-details}))
+                            (try (let [last-build (->> build-type-id
+                                                       (tc/last-builds server credentials)
+                                                       first)
+                                       build-type (->> build-type-id
+                                                       (tc/build-type server credentials)
+                                                       :attrs)
+                                       last-build-details (->> last-build
+                                                               :id
+                                                               (tc/build server credentials))]
+                                   {:last-build last-build
+                                    :build-type build-type
+                                    :last-build-details last-build-details})
+                                 (catch Exception e
+                                   (log/error e (format "cant get info for build type id=[%s]" build-type-id))
+                                   {:error (exception/pretty-print-exception e)})))
                           build-type-ids)]
     build-types))
 
