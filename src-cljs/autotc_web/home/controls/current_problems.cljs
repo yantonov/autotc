b6cbb6d606a3d14c65f0f645d5258360089e4933
Yury Antonov <yantonov@yandex.ru>
2016-06-01 07:49:41 +0500

    npe fix

--- src-cljs/autotc_web/home/controls/current_problems.cljs
+++ src-cljs/autotc_web/home/controls/current_problems.cljs
@@ -10,52 +10,49 @@
 (defn current-problems-list [server problems cursor]
   [:div
    nil
-   (map (fn [problem]
-          [:div
-           {:key (:name problem)}
+   (map
+    (fn [problem]
+      (let [stacktrace (or (:details problem) "")
+            test-name (:name problem)]
+        [:div {:key test-name}
+         [Row nil
+          [Col {:xs 6
+                :md 3
+                :class-name "single_problem"}
+           [:a {:href "#"
+                :on-click (fn [event]
+                            (copy/copy test-name)
+                            (.stopPropagation event))
+                :title "copy test name"}
+            [:img {:src "/img/copy.png"
+                   :class-name "copy_icon"
+                   :alt "test name"}]]
+           [:a {:href (:webUrl problem)
+                :target "_blank"
+                :title "test history"}
+            [:img {:src "/img/clock.png"
+                   :class-name "copy_icon"
+                   :alt "test history"}]]
+           [:a {:href (-> problem :build :webUrl)
+                :target "_blank"}
+            (->> problem :build :name)]]
+          [Col {:xs 18
+                :md 9}
+           [:a {:href "#"
+                :on-click (fn [event]
+                            (print test-name))
+                :class-name "current_problem_item"}
+            test-name]]]
+         (if (:show-stacktraces problems)
            [Row nil
-            [Col {:xs 6
-                  :md 3
-                  :class-name "single_problem"}
-             [:a {:href "#"
-                  :on-click (fn [event]
-                              (copy/copy (:name problem))
-                              (.stopPropagation event))
-                  :title "copy test name"}
-              [:img {:src "/img/copy.png"
-                     :class-name "copy_icon"
-                     :alt "test name"}]]
-             [:a {:href (:webUrl problem)
-                  :target "_blank"
-                  :title "test history"}
-              [:img {:src "/img/clock.png"
-                     :class-name "copy_icon"
-                     :alt "test history"}]]
-             [:a {:href (-> problem
-                            :build
-                            :webUrl)
-                  :target "_blank"}
-              (->> problem
-                   :build
-                   :name)]]
-            [Col {:xs 18
-                  :md 9}
-             [:a {:href "#"
-                  :on-click (fn [event]
-                              (print (:name problem))
-                              )
-                  :class-name "current_problem_item"}
-              (:name problem)]]]
-           (if (:show-stacktraces problems)
-             [Row nil
-              [:a {:href "#"
-                   :on-click (fn [event]
-                               (copy/copy (:details problem))
-                               (.stopPropagation event))
-                   :title "copy stack trace"
-                   :class-name "stacktrace_link"}
-               [:span {:class-name "stacktrace"}
-                (gstring/unescapeEntities
-                 (html/html-escape (:details problem)))]]]
-             nil)])
-        (:problems problems))])
+            [:a {:href "#"
+                 :on-click (fn [event]
+                             (copy/copy stacktrace)
+                             (.stopPropagation event))
+                 :title "copy stack trace"
+                 :class-name "stacktrace_link"}
+             [:span {:class-name "stacktrace"}
+              (gstring/unescapeEntities
+               (html/html-escape stacktrace))]]]
+           nil)]))
+    (:problems problems))])
