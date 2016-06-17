b5af0a38d3531d5a8f18475f784f1cab4c01b6f1
Yury Antonov <yantonov@yandex.ru>
2016-06-17 05:42:35 +0500

    display message in case of agent failure

--- src/autotc_web/models/tc.clj
+++ src/autotc_web/models/tc.clj
@@ -26,29 +26,35 @@
         build-types
         (doall (filter #(not (nil? %))
                        (map (fn [build-type-id]
-                              (try (let [last-build
-                                         (->> build-type-id
+                              (let [last-build
+                                    (try (->> build-type-id
                                               (tc/last-builds server credentials)
                                               parser/parse-last-builds
                                               first)
+                                         (catch Exception e
+                                           (log/error e)
+                                           {}))
 
-                                         build-type
-                                         (->> build-type-id
+                                    build-type
+                                    (try (->> build-type-id
                                               (tc/build-type server credentials)
                                               :attrs)
+                                         (catch Exception e
+                                           (log/error e)
+                                           {:name build-type-id}))
 
-                                         last-build-details
-                                         (->> last-build
+                                    last-build-details
+                                    (try (->> last-build
                                               :id
                                               (tc/build server credentials)
-                                              parser/parse-build-response)]
-                                     {:last-build last-build
-                                      :build-type build-type
-                                      :last-build-details last-build-details})
-                                   (catch Exception e
-                                     ;; TODO: display error for failed agents
-                                     (log/error e)
-                                     nil)))
+                                              parser/parse-build-response)
+                                         (catch Exception e
+                                           (log/error e)
+                                           {:status-text "Agent failure"}))]
+                                {:last-build last-build
+                                 :build-type build-type
+                                 :last-build-details last-build-details})
+                              )
                             build-type-ids)))
 
         vcs-roots-ids
