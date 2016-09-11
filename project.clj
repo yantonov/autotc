242ddc0e475d24ede845b22b4d643b2b1a7edc88
Yury Antonov <yantonov@yandex.ru>
2016-09-11 14:33:49 +0500

    bump up artifacts

--- project.clj
+++ project.clj
@@ -2,26 +2,26 @@
   :description "apply action to multiple teamcity agents"
   :url "https://github.com/yantonov/autotc"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.0"]
+                 [compojure "1.5.1"]
                  [ring-server "0.4.0"]
-                 [ring/ring-defaults "0.2.0"]
+                 [ring/ring-defaults "0.2.1"]
                  [hiccup "1.0.5"]
-                 [org.clojure/java.jdbc "0.5.8"]
+                 [org.clojure/java.jdbc "0.6.1"]
                  [org.xerial/sqlite-jdbc "3.8.11.2"]
                  [ring/ring-json "0.4.0"]
                  [ch.qos.logback/logback-classic "1.1.7"] ;logging
                  ;; cljs
-                 [org.clojure/clojurescript "1.8.51"]
+                 [org.clojure/clojurescript "1.9.229"]
                  [reagent "0.6.0-alpha" :exclusions [cljsjs/react]]
                  [cljsjs/react-bootstrap "0.28.1-0" :exclusions
                   [[org.webjars.bower/jquery]
                    [cljsjs/react-bootstrap]]]
-                 [cljs-ajax "0.5.4"]
+                 [cljs-ajax "0.5.8"]
                  ;; libs
                  [rex "0.1.0-SNAPSHOT"]
                  [clj-teamcity-api "0.1.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.9.7"]
-            [lein-cljsbuild "1.1.3"]]
+            [lein-cljsbuild "1.1.4"]]
   :ring {:handler autotc-web.handler/app
          :init autotc-web.handler/init
          :destroy autotc-web.handler/destroy}
@@ -31,7 +31,7 @@
                      :stacktraces? false
                      :auto-reload? false}}
              :dev {:dependencies [[ring-mock "0.1.5"]
-                                  [ring/ring-devel "1.4.0"]]}
+                                  [ring/ring-devel "1.5.0"]]}
              :profile {:jvm-opts ["-Dcom.sun.management.jmxremote"
                                   "-Dcom.sun.management.jmxremote.ssl=false"
                                   "-Dcom.sun.management.jmxremote.authenticate=false"
